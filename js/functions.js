f21405d8ae75ab9e1dbe8169053c20630f9a23d7
Gabriel Galang <galang69420@gmail.com>
2024-05-15 14:48:47 +0700

    container system and demo navbar

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+  function toggleDisplay(element) {
+    if (element.style.display == 'block') {
+         element.style.display = 'none';
+    } else {
+        element.style.display = 'block';
+    };
+};
