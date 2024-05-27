06b6bc85c19efb5f071ba49584a44393a62c67f7
Gabriel Galang <galang69420@gmail.com>
2024-05-27 21:59:19 +0700

    filter category

--- js/load.js
+++ js/load.js
@@ -51,3 +51,16 @@ fetch_master("../dataset/datasetSize.json").then(
         alert("Database Error!") 
     }
 );
+
+// Memuat data dari sebuah sumber daya (diasumsikan sebagai panggilan AJAX) dan memprosesnya
+fetch_master("../dataset/datasetPizzaCategory.json").then(
+    function (value) { 
+        // Ketika data berhasil dimuat, memasukkan data ke dalam variabel datasetMonth dan menandai bahwa dataset telah dimuat
+        dataset[4] = value; 
+        datasetLoaded = true;
+    },
+    function (error) { 
+        // Menampilkan pesan kesalahan jika terjadi kesalahan saat memuat data
+        alert("Database Error!") 
+    }
+);
