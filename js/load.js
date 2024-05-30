459170734fe66ad87ee33ae08ef3f631e334501a
Gabriel Galang <galang69420@gmail.com>
2024-05-30 23:11:43 +0700

    major revision 3

--- js/load.js
+++ js/load.js
@@ -6,10 +6,12 @@ fetch_master("../dataset/all.json").then(
         datasetLoaded = true;
         dataset[0] = processData(allData, 'order_hour');
         dataset[1] = processData(allData, 'order_day_week');
-        dataset[2] = processData(allData, 'order_month');
-        dataset[3] = processData(allData, 'pizza_size');
-        dataset[4] = processData(allData, 'pizza_category');
-        dataset[5] = processData(allData, 'pizza_type');
+        dataset[2] = processData(allData, 'order_day');
+        dataset[3] = processData(allData, 'order_month');
+        dataset[4] = processData(allData, 'order_quarter');
+        dataset[5] = processData(allData, 'pizza_size');
+        dataset[6] = processData(allData, 'pizza_category');
+        dataset[7] = processData(allData, 'pizza_type');
     },
     function (error) { 
         // Menampilkan pesan kesalahan jika terjadi kesalahan saat memuat data
